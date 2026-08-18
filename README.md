fadf9e6d13c3aaadb83446fcaec1b31e61a49046
bolachamole <atismistic@proton.me>
2026-08-17 22:25:37 -0400

    Configs atuais

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 Os diretórios devem ser movidos para ~/.config/
 
-Rodar ```git clone https://github.com/neovim/nvim-lspconfig ~/.config/nvim/pack/nvim/start/nvim-lspconfig```
-
 Para LSP de C/C++: ```dnf install clangd```
 
 Para LSP de Python: ```npm install -g pyright```
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,21 @@
 vim.opt.number = true
 vim.opt.expandtab = false
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+
+vim.pack.add({
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
+	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
+	{ src = "https://github.com/sainnhe/sonokai" },
+})
+
+require('gitsigns').setup()
 
 vim.lsp.enable('clangd')
 vim.lsp.enable('pyright')
+vim.lsp.enable('ts_ls')
 
 vim.diagnostic.config({
 	virtual_text = true,
@@ -10,5 +23,5 @@ vim.diagnostic.config({
 	underline = true,
 })
 
-vim.g.sonokai_style = 'atlantis'
+vim.g.sonokai_style = 'andromeda'
 vim.cmd.colorscheme('sonokai')
